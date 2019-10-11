a223c52ffbdc0522716969a2849ed49949353747
William Blacknall <williamblacknall@gmail.com>
2019-10-11 13:32:44 -0400

    Added if statements to request-logic.js to properly load webpage based on file extension

--- js/request-logic.js
+++ js/request-logic.js
@@ -5,4 +5,20 @@
 
 function getContentType(filename) {
   const extension = filename.match(/.*\.([^\.]*)$/)[1];
+
+  if (extension === "html"){
+    return "text/html";
+  }
+
+  if (extension === "css"){
+    return "text/css";
+  }
+
+  if (extension === "jpg" || extension === "jpeg"){
+    return "image/jpeg";
+  }
+
+  else{
+    return "text/plain"
+  }
 }
